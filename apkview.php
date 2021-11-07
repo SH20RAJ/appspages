52e792d679f0395fb94f31e90dbce99fddcfe428
sh20raj <shaswatraj3@gmail.com>
2021-11-07 12:28:31 +0530

    Apkview Upgaded

--- apkview.php
+++ apkview.php
@@ -8,6 +8,7 @@ $row = mysqli_fetch_array($result) ;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,77 +21,90 @@ $row = mysqli_fetch_array($result) ;
 
     <link rel="shortcut icon" href="<?php echo $row['ap_logo_url'] ?>" type="image/x-icon">
 </head>
+
 <body>
-<div class="container">
-    <header>
-        <h4>Appspages</h4>
-        <div class="links">
-            <a href="log/">Log</a>
-            <a href="submit/">Submit APK</a>
+<header>
+<h4>Appspages</h4>
+    <div class="links">
+        <a href="log/">Log</a>
+        <a href="submit/">Submit APK</a>
+    </div>
+</header>
+    <div class="container">
+        
+
+        <br><br>
+        <div class="logo">
+            <img src="<?php echo $row['ap_logo_url'] ?>" alt="" srcset="">
+        </div><br><br>
+
+        <div class="title">
+            <?php echo $row['ap_name'] ?>
         </div>
-    </header>
+        <br>
+        <div class="uploader center"><a
+                href="../tag/<?php echo $row['ap_uploader']?>"><?php echo $row['ap_uploader']?></a></div>
+        <br><br>
 
-    <br><br>
-    <div class="logo">
-        <img src="<?php echo $row['ap_logo_url'] ?>" alt="" srcset="">
-    </div><br><br>
+        <div class="download">
+            <a href="<?php echo $row['apk_link'] ?>"><button class="bttn-pill bttn-block bttn-primary">DOWNLOAD ( <?php echo $row['ap_size'] ?>mb )</button></a>
+        </div>
 
-    <div class="title">
-        <?php echo $row['ap_name'] ?>
-    </div>
-    <br>
-    <div class="uploader center"><a href="../tag/<?php echo $row['ap_uploader']?>"><?php echo $row['ap_uploader']?></a></div>
-    <br><br>
+        <br><br>
+        <p class="details">
+            <a class="bttn-material-flat bttn-md bttn-primary"
+                onclick="document.querySelector('#desc').classList.toggle('show')" data-toggle="collapse"
+                href="#desc" role="button" aria-expanded="false" aria-controls="collapseExample">
+                Show Description
+            </a>
+            <a class="bttn-float bttn-md bttn-primary">
+                Version : <?php echo $row['ap_version']?>
+            </a>
+            <a class="bttn-material-flat bttn-md bttn-primary"
+                onclick="document.querySelector('#ss').classList.toggle('show')" data-toggle="collapse"
+                href="#ss" role="button" aria-expanded="false" aria-controls="collapseExample">
+                Screenshots
+            </a>
+            <a class="bttn-float bttn-md bttn-primary">
+                Downloads : <?php echo $row['ap_downloads']?>
+            </a>
+            <a class="bttn-jelly bttn-md bttn-primary" style="border-radius:4px;">
+                <?php echo $row['ap_tag']?>
+            </a>
 
-    <div class="download">
-    <button class="bttn-pill bttn-block bttn-primary">DOWNLOAD ( <?php echo $row['ap_size'] ?>mb )</button>
-    </div>
+        </p>
+        <div class="collapse" id="desc">
+            <div class="card card-body">
 
-<br><br>
-<p class="details">
-  <a class="bttn-material-flat bttn-md bttn-primary" onclick="document.querySelector('#desc').classList.toggle('show')" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Show Description 
-  </a>
-  <a class="bttn-float bttn-md bttn-primary" >
-     Version : <?php echo $row['ap_version']?>
-  </a>
-  <?php  ?>
-  <a class="bttn-float bttn-md bttn-primary" >
-     Version : <?php echo $row['ap_version']?>
-  </a>
-</p>
-<div class="collapse" id="desc">
-  <div class="card card-body">
-
-<div class="description">
-    <?php echo nl2br($row['ap_description'])?>
-    </div>
-
-  </div>
-</div>
-
-    <br><br>
-    <div class="screenshots">
-        <h3>Screenshots </h3>
-        <pre class="sccon">
-        <?php 
-        $sc = explode('|', $row['ap_screenshots']);
-
-        for($x = 0; $x < count($sc); $x++) {
-        echo "<img class='screenshot' src='".$sc[$x]."'>";
-        }
-        ?>
-        </pre>
-    </div>
-    <br><br>
+                <div class="description">
+                    <?php echo nl2br($row['ap_description'])?>
+                </div>
+            </div>
+        </div>
+        <div class="fi">
+            <img src="<?php echo $row['ap_featured']?>" alt="" srcset="">
+        </div>
 
 
-    <div id="share">
+                <br><br>
+                <div class="screenshots collapse card card-body" id="ss">
+                    
+                    <pre class="sscon"><?php $sc = explode('|', $row['ap_screenshots']);for($x = 0; $x < count($sc); $x++) {echo "<img class='screenshot' src='".$sc[$x]."'>";}?>
+                    </pre>
+                </div>
+                <br><br>
 
-    </div>
+
+                <div id="share">
+
+                </div>
 
 
 
-</div>    
+</div>  
+<div class="footer">
+    <a class="a" href="">@AppsPages</a>
+</div>         
 </body>
+
 </html>
