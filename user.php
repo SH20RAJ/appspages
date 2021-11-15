d7de23735144fcb75b5618f672e6c2cf91671a08
sh20raj <shaswatraj3@gmail.com>
2021-11-15 13:04:21 +0530

    Update user.php

--- user.php
+++ user.php
@@ -13,6 +13,7 @@ if(isset($_GET['id'])){} else{header("location:../");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Apps - AppsPages</title>
     <link rel="stylesheet" href="../assets/bootstrap.min.css">
+    <link rel="shortcut icon" href="../logo.png" type="image/x-icon">
 </head>
 <body>
 <script src="../assets/autotopbar.js"></script>
