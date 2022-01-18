743f6acbf0ba82696544fde86d4e31a9886c2cf5
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-18 16:09:39 +0530

    Update fun.php

--- fun.php
+++ fun.php
@@ -1,6 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-header("Location: https://avatars.dicebear.com/api/adventurer/".uniqueid().".svg");
-die();
+
+<?php
+$id = uniqid();
+echo '<meta property="og:title" content="Here is your Avtar Image - You are a Doctor" />
+<meta property="og:description" content="Here is your Avtar Image..." />
+<meta property="og:image" content="https://avatars.dicebear.com/api/adventurer/'.$id.'.svg"" />
+<img src="https://avatars.dicebear.com/api/adventurer/'.$id.'.svg"/>';
+//header("Location: https://avatars.dicebear.com/api/adventurer/".$id.".svg");
 
 ?>
+<body>
+</body>
+</html>
+
