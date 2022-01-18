453fff633db4ac2aac98b551d6d22c4aa006baa7
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-18 16:14:36 +0530

    Update fun.php

--- fun.php
+++ fun.php
@@ -6,7 +6,7 @@
 $id = uniqid();
 echo '<meta property="og:title" content="Here is your Avtar Image - You are a Doctor" />
 <meta property="og:description" content="Here is your Avtar Image..." />
-<meta property="og:image" content="https://avatars.dicebear.com/api/adventurer/'.$id.'.svg"" />
+<meta property="og:image" content="https://avatars.dicebear.com/api/adventurer/'.$id.'.svg"/>
 <img src="https://avatars.dicebear.com/api/adventurer/'.$id.'.svg"/>';
 //header("Location: https://avatars.dicebear.com/api/adventurer/".$id.".svg");
 
